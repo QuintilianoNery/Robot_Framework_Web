a846d505e459391ddb04fc3466279d146281e5a7
Quintiliano Paquini Nery <quintiliano1995@gmail.com>
2024-11-28 13:53:05 -0300

    Criação de usuário no banco de dados

--- resources/libs/database.py
+++ resources/libs/database.py
@@ -9,3 +9,16 @@ def remove_user(email):
     users = db['users']
     users.delete_many({'email': email})
     print('removing user by ' + email)
+
+@keyword('Incluir usuario no banco de dados')
+def insert_user(name, email, password):
+    doc = {
+        'name': name,
+        'email': email,
+        'password': password
+    }
+    users = db['users']
+    users.insert_one(doc)
+    import json
+    # print('insert user' + json.dumps(doc))
+    print(f'insert user {doc}')
